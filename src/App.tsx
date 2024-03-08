387783e730dd00d1457f95deccc317c1d77c7507
Vinicius-Academico <158180985+Vinicius-Academico@users.noreply.github.com>
2024-03-07 20:30:53 -0400

    :lipstick: aprendendo alguns padrões

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ function App() {
 
   return (
     <>
-
+      <div>
+        <h1 className="text-2xl text-center text-violet-700 bg-pink-400/50 bg-brand p-2 rounded shadow-lg hover:bg-emerald-300">Hello World</h1>
+        <div>
+          <div className="flex">
+            <div className="w-10 h-10 bg-emerald-400"></div>
+            <div className="w-10 h-10 bg-violet-400"></div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
