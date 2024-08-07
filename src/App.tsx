9b7779123933431b2f5045ce34e9de15400c1e70
Vinicius Dhenner <viniciusefisim@gmail.com>
2024-08-07 15:27:19 -0400

    Adiciionando apresentação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { NavBar } from "./components/navbar"
+import { Presentation } from "./components/presentation"
 
 function App() {
   return (
     <>
       <div>
         <NavBar/> 
+        <Presentation/>
       </div>
     </>
   )
