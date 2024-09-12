8829bcbf66f80eab5efbeec8da5948e54aa3a2ef
Vinicius-Academico <158180985+Vinicius-Academico@users.noreply.github.com>
2024-09-12 19:49:57 -0400

    imagem de perfil

--- src/components/presentation/index.tsx
+++ src/components/presentation/index.tsx
@@ -4,7 +4,7 @@ export function Presentation() {
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            src="https://avatars.githubusercontent.com/u/169805885?v=4"
             className="max-w-sm rounded-lg shadow-2xl" />
           <div>
             <h1 className="text-5xl font-bold">Vinicius Dhenner Marins de Oliveira</h1>
